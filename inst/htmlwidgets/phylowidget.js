2d98c44d3749d71b629abd1df5cc10b9b1be0040
timelyportfolio <kent.russell@timelyportfolio.com>
2015-02-10 17:38:03 -0600

    demonstrate a sizing option

--- inst/htmlwidgets/phylowidget.js
+++ inst/htmlwidgets/phylowidget.js
@@ -9,9 +9,9 @@ HTMLWidgets.widget({
     var svg=d3.select(el).append("svg")
      .attr("width",width)
      .attr("height",height);
-     
+
     var tree=d3.layout.phylotree(el).size ([height, width]).separation (function (a,b) {return 0;});
-  
+
     return {"svg": svg, "tree": tree};
   },
 
@@ -27,17 +27,30 @@ HTMLWidgets.widget({
         .node_circle_size (4);
 
     tree(d3_phylotree_newick_parser(newick_string)).svg(svg).layout();
+
+    this.makeResponsive( el )
   },
 
   resize: function(el, width, height, instance) {
     var svg=d3.select("#"+el.id+" svg")
      .attr("width",width)
      .attr("height",height);
-     
+
       instance.tree.size ([height, width]).layout();
+  },
+
+      // use this to sort of make our diagram responsive
+    //  or at a minimum fit within the bounds set by htmlwidgets
+    //  for the parent container
+  makeResponsive: function(el){
+     var svg = el.getElementsByTagName("svg")[0];
+     if(svg){
+      svg.setAttribute("viewBox", "0 0 " + svg.getAttribute("width") + " " + svg.getAttribute("height"))
+      if(svg.width) {svg.removeAttribute("width")};
+      if(svg.height) {svg.removeAttribute("height")};
+      svg.style.width = "100%";
+      svg.style.height = "100%";
+     }
   }
-  
- 
-    
 
 });
